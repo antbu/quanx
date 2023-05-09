439131216bfb0eac148abc4d8a58152034353f9a
Martin Lee <318183437@qq.com>
2023-05-10 03:50:21 +0800

    2

--- Script/ql_sync.js
+++ Script/ql_sync.js
@@ -175,12 +175,9 @@ function QLSync(url, username, password) {
         const options = {
           url: url,
           method: method,
-          headers: {
-            'Accept': 'application/json',
-          }
         };
         if (data) {
-          options.body = data;
+          options.body = $.toStr(data);
           options.headers['Content-Type'] = 'application/json;charset=UTF-8';
 
         }
@@ -205,12 +202,10 @@ function QLSync(url, username, password) {
       return new Promise(async (resolve, reject) => {
         try {
         $.log('2',this.username,this.password)
-
-          const time = new Date().getTime()
-          const resp = await this.ajax("POST", `${this.url}/api/user/login&t=${time}`,{
+          const resp = await this.ajax("POST", `${this.url}/api/user/login`,{
             "username": this.username,
             "password": this.password
-        }, false);
+        }, false).then().catch(e => $.log('22',e));
         $.log('2-resp',resp.data.token)
 
           this.token = resp.data.token;
